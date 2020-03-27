f23a66dcbaae4ecfd68d6136d6f41b8ff17f5e02
Kristoffer Bråthen Halden <>
2020-03-27 13:43:37 +0100

    Hooked up components

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,9 +3,10 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+import ListGames from '../games/ListGames.js'
 
 export default function (props) {
     return (
-        <div></div>
+        <ListGames></ListGames>
     )
 }
